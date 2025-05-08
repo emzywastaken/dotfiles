9d84d8468b07e5466e5414f25e19890ea03c007c
emzywastaken <amiamemetoo@gmail.com>
2025-05-08 20:15:21 +0100

    Move from zsh config to home-manager options
    - Stopped using zinit plugin manager

--- user/zsh/zsh.nix
+++ user/zsh/zsh.nix
@@ -1,7 +1,48 @@
 {  pkgs, ... }:
 
 {
-  home.file.".zshrc".source = ./.zshrc;
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+
+    shellAliases = {
+      ls = "ls --color";
+      ll = "ls -lh";
+      la = "ls -lAh";
+      lvim = "NVIM_APPNAME=lvim nvim";
+    };
+
+    history = rec {
+      append = true;
+      findNoDups = true;
+      ignoreDups = true;
+      ignoreAllDups = true;
+      ignoreSpace = true;
+      saveNoDups = true;
+      share = true;
+      save = size;
+      size = 5000;
+    };
+
+    plugins = [
+      { name = "fzf-tab"; src = pkgs.zsh-fzf-tab.src; }
+    ];
+
+    initContent = ''
+      HISTDUP=erase
+
+      # Completion styling
+      zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
+      zstyle ':completion:*' list-colors "''${(s.:.)LS_COLORS}"
+      zstyle ':completion:*' menu no
+
+      eval "$(${pkgs.starship}/bin/starship init zsh)"
+      eval "$(${pkgs.fzf}/bin/fzf --zsh)"
+    '';
+  };
+
   home.packages =  [
     pkgs.starship
   ];
