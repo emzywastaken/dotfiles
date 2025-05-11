0c9175755be815fa4315792ed43d343bb6c065c1
emzywastaken <amiamemetoo@gmail.com>
2025-05-11 19:29:10 +0100

    Enabled nvidia optimus prime
    - Before this change if i enabled 'hybrid graphics' in bios the laptop
    would boot to a blank screen. This change allows the iGpu and dGpu to be
    used at the same time, preferring the iGpu for light workloads hence
    lower power consumption
    - The default mode is offload but there is a `sync` specialisation that
    enables the dGpu
    - Also added some hardware accelerated video drivers that adds some
    addition power savings

--- hosts/nixos-btw/configuration.nix
+++ hosts/nixos-btw/configuration.nix
@@ -48,10 +48,44 @@
     '';
   };
 
-  hardware.nvidia = {
-    modesetting.enable = true;
-    powerManagement.enable = true;
-    open = true;
+  hardware = {
+    graphics = {
+      enable = true;
+      enable32Bit = true;
+      extraPackages = with pkgs; [
+        intel-media-driver
+        intel-vaapi-driver
+        libvdpau-va-gl
+      ];
+    };
+    nvidia = {
+      modesetting.enable = true;
+      powerManagement.enable = true;
+      powerManagement.finegrained = true;
+      open = true;
+
+      prime = {
+        offload = {
+          enable = true;
+          enableOffloadCmd = true;
+        };
+        intelBusId = "PCI:0:2:0";
+        nvidiaBusId = "PCI:1:0:0";
+      };
+    };
+  };
+
+  specialisation = {
+    gaming.configuration = {
+      hardware.nvidia = {
+        powerManagement.finegrained = lib.mkForce false;
+        prime.sync.enable = lib.mkForce true;
+        prime.offload = {
+          enable = lib.mkForce false;
+          enableOffloadCmd = lib.mkForce false;
+        };
+      };
+    };
   };
 
   # Configure keymap in X11
