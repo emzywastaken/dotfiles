2c2829248d3c1d0c7d6bf7c4b64db0ed92815dab
emzywastaken <amiamemetoo@gmail.com>
2025-06-12 00:48:50 +0100

    Add `withStarshipPrompt` option to fish module and more
    - Rebind `cfg` to `config.modules.fish` as its shorter.
    - Add new `withStarshipPrompt` option that allows fish prompt to be
    configured with starship.
    - Now set `SHELL` env var if `defaultInteractiveShell` is enabled,
    mainly because nix shell uses it to determine the shell to use.
    - Enable fzf shell integration.
    - Add 'lvim' shell alias. Aliased to nvim but with lazyvim config.

--- user/shell/fish.nix
+++ user/shell/fish.nix
@@ -4,18 +4,29 @@
   config,
   ...
 }: let
-  cfg = config.modules;
+  cfg = config.modules.fish;
 in {
   options.modules = {
     fish.enable = lib.mkEnableOption "Whether to enable custom fish module";
+    fish.withStarshipPrompt = lib.mkEnableOption "Configure prompt with starship";
     fish.defaultInteractiveShell = lib.mkEnableOption "Whether to make fish the default interactive shell";
   };
 
-  config = lib.mkIf cfg.fish.enable {
+  config = lib.mkIf cfg.enable {
     programs.fish.enable = true;
+
+    programs.fish.interactiveShellInit = lib.mkIf cfg.withStarshipPrompt
+      "eval (${pkgs.starship}/bin/starship init fish)";
+    programs.fish.shellAbbrs.lvim = "NVIM_APPNAME=lvim nvim";
+
     # make fish default interactive shell
-    programs.kitty = lib.mkIf cfg.fish.defaultInteractiveShell {
+    programs.kitty = lib.mkIf cfg.defaultInteractiveShell {
       settings.shell = "${lib.getExe pkgs.fish}";
     };
+
+    home.sessionVariables.SHELL = lib.mkIf cfg.defaultInteractiveShell "fish";
+
+    programs.fzf.enable = true;
+    programs.fzf.enableFishIntegration = true;
   };
 }
