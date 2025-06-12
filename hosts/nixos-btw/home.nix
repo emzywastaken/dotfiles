aaf711928cbf03c617c009a8563511d638e85f30
emzywastaken <amiamemetoo@gmail.com>
2025-06-12 21:45:31 +0100

    Add monaspace font

--- hosts/nixos-btw/home.nix
+++ hosts/nixos-btw/home.nix
@@ -133,18 +133,7 @@ in {
     zathura
     zvm
 
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
+    nerd-fonts.monaspace
   ];
 
   # Home Manager can also manage your environment variables through
