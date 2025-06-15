1b9e0bf2c04ca893b022ad8c1a8c98815fb41c60
emzywastaken <amiamemetoo@gmail.com>
2025-06-15 23:45:53 +0100

    Update sound configuration

--- hosts/nixos-btw/configuration.nix
+++ hosts/nixos-btw/configuration.nix
@@ -115,11 +115,13 @@ in {
   hardware.openrazer.users = ["emzy"];
 
   # Enable sound.
-  # hardware.pulseaudio.enable = true;
-  # OR
+  security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
     pulse.enable = true;
+    jack.enable = true;
   };
 
   # Enable touchpad support (enabled default in most desktopManager).
