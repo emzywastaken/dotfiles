fe037fe5e0c488f648f1acae04ce9c09323a8e17
emzywastaken <amiamemetoo@gmail.com>
2025-06-19 23:05:14 +0100

    Change rofi theme to gruvbox
    - Also intalled some other themes to rofi themes directory.

--- user/rofi/rofi.nix
+++ user/rofi/rofi.nix
@@ -1,8 +1,12 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  themesDir = "rofi/themes";
+in {
   programs.rofi = {
     enable = true;
     package = pkgs.rofi-wayland;
     terminal = "${pkgs.kitty}/bin/kitty";
-    theme = ./themes/tokyonight.rasi;
+    theme = ./themes/gruvbox-material.rasi;
   };
+  xdg.dataFile."${themesDir}/squared-everforest.rasi".source = ./themes/squared-everforest.rasi;
+  xdg.dataFile."${themesDir}/tokyonight.rasi".source = ./themes/tokyonight.rasi;
 }
