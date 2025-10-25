afa95aeab6abcba530a64d9f05dcae4e18e38c2e
emzywastaken <amiamemetoo@gmail.com>
2025-10-25 16:12:40 +0100

    force intel Xe driver for ink

--- hosts/ink/default.nix
+++ hosts/ink/default.nix
@@ -23,6 +23,8 @@
 
   networking.hostName = "ink";
 
+  boot.kernelParams = ["i915.force_probe=!7d55" "xe.force_probe=7d55"];
+
   hardware.graphics = {
     extraPackages = with pkgs; [
       intel-media-driver
