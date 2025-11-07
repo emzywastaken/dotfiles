55e33feb3f2d2cbd34d38c5fefbe05b59bf54bcc
emzywastaken <amiamemetoo@gmail.com>
2025-11-07 19:58:16 +0100

    fix lazygit config
    - lazygit replaced the `paging` key with an array `pagers`

--- user/lazygit/default.nix
+++ user/lazygit/default.nix
@@ -14,10 +14,12 @@ in {
     programs.lazygit = {
       enable = true;
       settings = {
-        git.paging = {
-          colorArg = "always";
-          pager = "${pkgs.delta}/bin/delta --dark --paging=never";
-        };
+        git.pagers = [
+          {
+            colorArg = "always";
+            pager = "${pkgs.delta}/bin/delta --dark --paging=never";
+          }
+        ];
       };
     };
   };
