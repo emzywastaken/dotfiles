12ff4f991910b991b96075ac854ef2cdf29c3192
emzywastaken <amiamemetoo@gmail.com>
2026-01-01 17:59:45 +0100

    update hyprland window rule syntax and disable color management
    cm_enabled has wierd colors and cba to confire it

--- user/wms/hyprland/default.nix
+++ user/wms/hyprland/default.nix
@@ -65,6 +65,10 @@ in {
           layout = "dwindle";
         };
 
+        render = {
+          cm_enabled = false;
+        };
+
         decoration = {
           rounding = 10;
           rounding_power = 2;
@@ -160,14 +164,14 @@ in {
         };
 
         windowrule = [
-          "float, class:xdg-desktop-portal-gtk"
+          "float on, match:class xdg-desktop-portal-gtk"
           # Bitwarden popup
-          "float, class:chrome-nngceckbapebfimnlniiiahkandclblb-Default"
-          "immediate, class:^(cs2)$"
+          "float on, match:class chrome-nngceckbapebfimnlniiiahkandclblb-Default"
+          "immediate on, match:class ^(cs2)$"
           # Ignore maximize requests from apps.
-          "suppressevent maximize, class:.*"
+          "suppress_event maximize, match:class .*"
           # Fix some dragging issues with XWayland
-          "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
+          "no_focus on, match:class ^$, match:title ^$, match:xwayland 1, match:float 1, match:fullscreen 0, match:pin 0"
         ];
       };
 
