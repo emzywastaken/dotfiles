94e61121e8244aa0126afc0d6a082409bcbe4479
emzywastaken <amiamemetoo@gmail.com>
2026-01-27 15:37:34 +0100

    disable systemd unit management for hjem
    interferes with home-manager when enabled

--- modules/home.nix
+++ modules/home.nix
@@ -3,6 +3,10 @@
 
   hjem = {
     clobberByDefault = true;
-    users.emzy.enable = true;
+    users.emzy = {
+      enable = true;
+      # this interferes with home-manager's systemd unit management
+      systemd.enable = false;
+    };
   };
 }
